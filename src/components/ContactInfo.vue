956fbc8da15ec383adbcb9d2b6a773d60683ba51
halil fahriza <halilfahriza03@gmail.com>
2024-04-28 17:29:58 +0700

    first commit

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'ContactInfo',
+});
+
+defineProps({
+    header: String,
+});
+
+const data = reactive([
+    {
+        icon: 'mdi-cellphone-sound',
+        title: 'Phone',
+        value: '+62 89623048147',
+        href: 'tel:+6289623048147',
+        target: null,
+    },
+    {
+        icon: 'mdi-gmail',
+        title: 'Gmail',
+        value: 'halilfahriza03@gmail.com',
+        href: 'halil:halilfahriza03@gmail.com',
+        target: null,
+    },
+    {
+        icon: 'mdi-github',
+        title: 'GitHub',
+        value: 'halilfahriza',
+        href: 'https://github.com/halilfahriza',
+        target: '_blank',
+    },
+    
+]);
+</script>
+
+<template>
+    <div class="contact-info">
+        <h5 class="text-h5 text-uppercase text-deep-purple-accent-1">{{ header }}</h5>
+        <div class="contact-info__item mt-3" v-for="(item, i) in data" :key="i">
+            <div class="contact-info__icon">
+                <v-icon size="x-large">{{ item.icon }}</v-icon>
+            </div>
+            <div class="contact-info__content">
+                <span class="text-uppercase text-button">{{ item.title }}</span>
+                <a :href="item.href" :target="item.target">{{ item.value }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.contact-info__item {
+    display: flex;
+    align-items: flex-end;
+}
+
+.contact-info__content {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+}
+</style>
